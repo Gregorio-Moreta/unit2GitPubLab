ea435ce7824bf757a3cd3b890a429040dde6ddd2
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-09-21 21:38:29 -0400

    Used module.exports to export data from ./models/drinks.js to my server.js file. I then created an app.get(/drinks) route so that I could res.send(drinks) which would display all the drinks data as json in the browser.

--- server.js
+++ server.js
@@ -2,11 +2,17 @@
 const express = require('express');
 //  Creating the application object
 const app = express();
-
-app.get('/drinks', (req, res) => {
-    res.send('welcome to the Gitpub App!')
+//  Creating a variable to store my drinks data from /models/drinks.js
+const drinks = require("./models/drinks.js")
+//  Creating a drinks route to send a message to the broswer
+app.get('/homePage', (req, res) => {
+    res.send('Welcome to the Gitpub App!')
 })
-
+//  Create a get route /drinksthat will res.send(drinks), which will display your drinks data as json in the browser
+app.get('/drinks', (req, res) => {
+    res.send(drinks)
+})
+//  Set the server to listen at port 7000 
 app.listen(7000, () => {
     console.log('listening');
 })
