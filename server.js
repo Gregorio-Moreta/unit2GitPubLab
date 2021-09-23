5f2db8cd706a32cd1261ee58fdb5cfd169189b6a
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-09-23 03:09:02 -0400

    I created an unorderlist of the drinks which is rendered dynamically through ejs using data from my drinks.js database. I also capitalized the first letter of the names of the drinks, but not the first letter of each word.

--- server.js
+++ server.js
@@ -3,26 +3,16 @@ const express = require('express');
 //  Creating the application object
 const app = express();
 //  Creating a variable to store my drinks data from /models/drinks.js
-const drinks = require("./models/drinks.js")
+const drinks = require ("./models/drinks")
 //  Creating a drinks route to send a message to the broswer
 app.get('/homePage', (req, res) => {
     res.send('Welcome to the Gitpub App!')
 })
 //  Create a get route /drinksthat will res.send(drinks), which will display your drinks data as json in the browser
 app.get('/drinks', (req, res) => {
-    res.render('drinks_index.ejs')
+    res.render('drinks_index.ejs', {allDrinks: drinks} )
 })
 //  Set the server to listen at port 7000 
 app.listen(7000, () => {
     console.log('listening');
 })
-
-
-
-
-
-
-
-
-
-
