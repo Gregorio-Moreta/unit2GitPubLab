e53fc54238d798b424093c187b2f00204aba4816
Gregorio Moreta <gregoriomoreta4@gmail.com>
2021-11-05 18:09:24 -0400

    got the show route working, displaying name, price, and image, except I still need to capitalize the names in this route. Index route is done.

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const express = require('express');
 const app = express();
 //  Creating a variable to store my drinks data from /models/drinks.js
 const drinks = require ("./models/drinks")
+///////////////////////////////////
+// MIDDLEWARE
+///////////////////////////////////
+
+// near the top, around other app.use() call
+// extended property is a body parser
+// Parse Request Bodies if Content-Type Header is: application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+
 //  Creating a drinks route to send a message to the broswer
 app.get('/homePage', (req, res) => {
     res.send('Welcome to the Gitpub App!')
@@ -15,8 +24,8 @@ app.get('/drinks', (req, res) => {
 })
 //  Create a get route /drinks/:id
 app.get('/drinks/:id', (req, res) => {
-    res.send(req.params.id)
-})
+    res.render('drinks_show.ejs', {drink: drinks[req.params.id], title: 'first - index page'});
+});
 //  Set the server to listen at port 7000 
 app.listen(8000, () => {
     console.log('listening');
